1b7293ccaec1eab656a9b85676f44be256e1a6a5
pip3campos <felipeacampose@gmail.com>
2024-01-02 00:45:23 -0300

    cambios en sessions model y data

--- models/Session.js
+++ models/Session.js
@@ -2,15 +2,11 @@ import { Schema, model, Types } from "mongoose";
 let collection = 'sessions'
 let schema = new Schema({
     date:{type:Date, required:true},
-    duration:{type:String, required:true},
+    duration:{type:Number, required:true},
+    category:{type:String, required:true},
     level:{type:String, required:true},
-    price:{type:Number, required:true},
-    wall:{type:String, required:true},
     instructor_id:{type:Types.ObjectId, required:true},
-    description:{type:String},
-    location:{type:String},
-    spots:{type:Number, required:true},
-    status:{type:String}
+    spots:{type:Number, required:true}
 },{timestamps:true})
 const Session = model(collection, schema)
 export default Session
