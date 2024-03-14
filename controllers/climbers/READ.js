2bd99a6afaafa9ae84791049d726af91d19b75e7
pip3campos <felipeacampose@gmail.com>
2024-03-14 16:40:53 -0300

    READ climbers, register schema

--- controllers/climbers/READ.js
+++ controllers/climbers/READ.js
@@ -1,17 +1,33 @@
 import Climber from '../../models/Climber.js'
 
-async function getClimbers(req,res,next){
-    const { email } = req.query
+async function getClimbers(req,res){
     let queries = {}
+    let pagination = {page:1 , limit:5}
+    let next=false
+    let prev=false
+    if (req.query.page) {pagination.page = req.query.page}
+    if (req.query.quantity) {pagination.limit = req.query.quantity}
+    /* const { email } = req.query
     if (email) {
         queries.email = email
-    }
+    } */
     try {
         const allClimbers = await Climber.find(queries)
+            .skip(pagination.page > 0 ? (pagination.page-1) * pagination.limit : 0)
+            .limit(pagination.limit > 0 ? pagination.limit : 0)
+        const total = ( await Climber.find({}) ).length
+        let maxPages = ( total / pagination.limit) .toFixed()
+        if ( maxPages > pagination.page ) { next = true }
+        if ( pagination.page > 1 ) { prev = true }
         return res.status(200).json({
             success: true,
             response: allClimbers,
-            message: "Here are all the climbers registered"
+            message: "Climbers registered",
+            pages: {
+                maxPages,
+                prev,
+                next
+            }
         })
         
     } catch (error) {
--- schemas/registerSchema.js
+++ schemas/registerSchema.js
@@ -2,9 +2,7 @@ import joi from "joi"
 
 const registerValidator = joi.object({
     email: joi.string().required(),
-    password: joi.string().required(),
-    phone: joi.string().required(),
-    photo: joi.string().required()
+    password: joi.string().required()
 })
 
 export default registerValidator
